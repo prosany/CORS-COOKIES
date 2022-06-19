e589f14594d7605f0f97a18bfcdd9a239d947540
Mahabub Hasan Sany <74856571+prosany@users.noreply.github.com>
2022-06-19 23:40:22 +0600

    Okay

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var incomingData = document.querySelector("#incomingData");
 
 const loadData = () => {
-  fetch("http://localhost:3000/data")
+  fetch("https://cors-and-cookies.herokuapp.com/data")
     .then((response) => response.json())
     .then((data) => {
       incomingData.innerHTML = JSON.stringify(data);
